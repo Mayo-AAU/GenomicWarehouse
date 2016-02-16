b5e33c581a0743c1beab5d1a858a203db34ca1b0
Daniel Blezek <blezek.daniel@mayo.edu>
2016-02-16 08:48:28 -0600

    Updated to later API for HBase

--- src/test/java/edu/mayo/hadoop/commons/examples/HBaseITCase.java
+++ src/test/java/edu/mayo/hadoop/commons/examples/HBaseITCase.java
@@ -1,43 +1,50 @@
 package edu.mayo.hadoop.commons.examples;
 
-import com.github.sakserv.minicluster.config.ConfigVars;
-import edu.mayo.hadoop.commons.hbase.HBaseConnector;
-import edu.mayo.hadoop.commons.hbase.HBaseUtil;
-import edu.mayo.hadoop.commons.minicluster.MiniClusterUtil;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.Properties;
 
-import java.io.IOException;
+import com.github.sakserv.minicluster.config.ConfigVars;
 
-import static org.junit.Assert.assertEquals;
+import edu.mayo.hadoop.commons.hbase.HBaseConnector;
+import edu.mayo.hadoop.commons.hbase.HBaseUtil;
+import edu.mayo.hadoop.commons.minicluster.MiniClusterUtil;
 
 public class HBaseITCase {
 
     // Logger
     private static final Logger LOG = LoggerFactory.getLogger(HBaseITCase.class);
-    //properties file
-    public static final String HBASE_PROPS = "target/test-classes/hbase.properties";
+    // properties file
+    public static final String HBASE_PROPS = "hbase.properties";
     private static Properties props;
 
     static {
-        try {
-            props = MiniClusterUtil.loadPropertiesFile(HBASE_PROPS);
-        } catch(IOException e) {
+        try (InputStream stream = HBaseITCase.class.getResourceAsStream(HBASE_PROPS)) {
+            props = MiniClusterUtil.loadPropertiesStream(stream);
+        } catch (IOException e) {
             LOG.error("Unable to load property file: {}", HBASE_PROPS);
         }
     }
-
-
 
     @BeforeClass
     public static void setUp() throws Exception {
@@ -65,33 +72,30 @@ public class HBaseITCase {
         String colQualiferName = props.getProperty(ConfigVars.HBASE_TEST_COL_QUALIFIER_NAME_KEY);
         Integer numRowsToPut = Integer.parseInt(props.getProperty(ConfigVars.HBASE_TEST_NUM_ROWS_TO_PUT_KEY));
 
-
         LOG.info("HBASE: Creating table {} with column family {}", tableName, colFamName);
         createHbaseTable(tableName, colFamName, configuration);
 
         LOG.info("HBASE: Populate the table with {} rows.", numRowsToPut);
-        for (int i=0; i<numRowsToPut; i++) {
+        for (int i = 0; i < numRowsToPut; i++) {
             putRow(tableName, colFamName, String.valueOf(i), colQualiferName, "row_" + i, configuration);
         }
 
         LOG.info("HBASE: Fetching and comparing the results");
-        for (int i=0; i<numRowsToPut; i++) {
+        for (int i = 0; i < numRowsToPut; i++) {
             Result result = getRow(tableName, colFamName, String.valueOf(i), colQualiferName, configuration);
             assertEquals("row_" + i, new String(result.value()));
         }
 
         LOG.info("Test complete, dropping schema!");
         hutil.dropAll();
-        //shutdown client
+        // shutdown client
         hcon.close();
-
 
     }
 
-    private static void createHbaseTable(String tableName, String colFamily,
-                                         Configuration configuration) throws Exception {
-
-        final HBaseAdmin admin = new HBaseAdmin(configuration);
+    private static void createHbaseTable(String tableName, String colFamily, Configuration configuration) throws Exception {
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        final Admin admin = connection.getAdmin();
         HTableDescriptor hTableDescriptor = new HTableDescriptor(TableName.valueOf(tableName));
         HColumnDescriptor hColumnDescriptor = new HColumnDescriptor(colFamily);
 
@@ -99,20 +103,19 @@ public class HBaseITCase {
         admin.createTable(hTableDescriptor);
     }
 
-    private static void putRow(String tableName, String colFamName, String rowKey, String colQualifier, String value,
-                               Configuration configuration) throws Exception {
-        HTable table = new HTable(configuration, tableName);
+    private static void putRow(String tableName, String colFamName, String rowKey, String colQualifier, String value, Configuration configuration) throws Exception {
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        Table table = connection.getTable(TableName.valueOf(tableName));
         Put put = new Put(Bytes.toBytes(rowKey));
-        put.add(Bytes.toBytes(colFamName), Bytes.toBytes(colQualifier), Bytes.toBytes(value));
+        put.addColumn(Bytes.toBytes(colFamName), Bytes.toBytes(colQualifier), Bytes.toBytes(value));
         table.put(put);
-        table.flushCommits();
         table.close();
     }
 
-    private static Result getRow(String tableName, String colFamName, String rowKey, String colQualifier,
-                                 Configuration configuration) throws Exception {
+    private static Result getRow(String tableName, String colFamName, String rowKey, String colQualifier, Configuration configuration) throws Exception {
         Result result;
-        HTable table = new HTable(configuration, tableName);
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        Table table = connection.getTable(TableName.valueOf(tableName));
         Get get = new Get(Bytes.toBytes(rowKey));
         get.addColumn(Bytes.toBytes(colFamName), Bytes.toBytes(colQualifier));
         get.setMaxVersions(1);
