6617293fc56e715beddbfc6947238b560329c124
Daniel Blezek <blezek.daniel@mayo.edu>
2016-02-16 08:47:52 -0600

    Moved files into a proper directory structure

--- src/main/java/edu/mayo/hadoop/commons/examples/hbase/MyHBaseDAO.java
+++ src/main/java/edu/mayo/hadoop/commons/examples/hbase/MyHBaseDAO.java
@@ -2,7 +2,6 @@ package edu.mayo.hadoop.commons.examples.hbase;
 
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -12,8 +11,7 @@ import org.apache.hadoop.hbase.util.Bytes;
  */
 public class MyHBaseDAO {
 
-    public static void insertRecord(Connection c, String tableName, HBaseTestObj obj)
-            throws Exception {
+    public static void insertRecord(Connection c, String tableName, HBaseTestObj obj) throws Exception {
         Table table = c.getTable(TableName.valueOf(tableName));
         Put put = createPut(obj);
         table.put(put);
@@ -21,10 +19,8 @@ public class MyHBaseDAO {
 
     public static Put createPut(HBaseTestObj obj) {
         Put put = new Put(Bytes.toBytes(obj.getRowKey()));
-        put.addColumn(Bytes.toBytes("CF"), Bytes.toBytes("CQ-1"),
-                Bytes.toBytes(obj.getData1()));
-        put.addColumn(Bytes.toBytes("CF"), Bytes.toBytes("CQ-2"),
-                Bytes.toBytes(obj.getData2()));
+        put.addColumn(Bytes.toBytes("CF"), Bytes.toBytes("CQ-1"), Bytes.toBytes(obj.getData1()));
+        put.addColumn(Bytes.toBytes("CF"), Bytes.toBytes("CQ-2"), Bytes.toBytes(obj.getData2()));
         return put;
     }
 }
