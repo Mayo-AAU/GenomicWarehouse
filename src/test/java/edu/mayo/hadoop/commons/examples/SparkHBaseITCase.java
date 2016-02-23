783694e812100035085c9d661e5ff13aaf2b57f9
drachimera <daniel.quest@gmail.com>
2016-02-23 15:19:06 -0600

    Starting some code to test the spark-hbase connection

--- src/test/java/edu/mayo/hadoop/commons/examples/SparkHBaseITCase.java
+++ src/test/java/edu/mayo/hadoop/commons/examples/SparkHBaseITCase.java
@@ -1,5 +1,17 @@
 package edu.mayo.hadoop.commons.examples;
 
+import it.nerdammer.spark.hbase.*;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.rdd.RDD;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+import static java.util.stream.IntStream.*;
+
 /**
  * Created by m102417 on 2/15/16.
  *
@@ -8,4 +20,21 @@ package edu.mayo.hadoop.commons.examples;
  *
  */
 public class SparkHBaseITCase {
+
+
+    @Test
+    public void testConnect(){
+
+        //make a connection on localhost to spark
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        ArrayList values = new ArrayList();
+        for(int i=0;i<100;i++){values.add(i);}
+        //RDD rdd = sc.parallelize(values).map(i -> (i.toString(), i+1, "Hello"))
+
+
+
+    }
+
 }
