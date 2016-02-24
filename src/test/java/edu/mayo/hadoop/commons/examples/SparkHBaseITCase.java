3c7ca2ae721e788fa303df86667e9da3e384a90d
drachimera <daniel.quest@gmail.com>
2016-02-24 10:36:47 -0600

    fixing build issue with hbase cluster

--- src/test/java/edu/mayo/hadoop/commons/examples/SparkHBaseITCase.java
+++ src/test/java/edu/mayo/hadoop/commons/examples/SparkHBaseITCase.java
@@ -1,13 +1,21 @@
 package edu.mayo.hadoop.commons.examples;
 
+import edu.mayo.hadoop.commons.minicluster.MiniClusterUtil;
 import it.nerdammer.spark.hbase.*;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.rdd.RDD;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.stream.IntStream;
 
 import static java.util.stream.IntStream.*;
@@ -22,8 +30,27 @@ import static java.util.stream.IntStream.*;
 public class SparkHBaseITCase {
 
 
+    // Logger
+    private static final Logger LOG = LoggerFactory.getLogger(SparkHBaseITCase.class);
+    // properties file
+    public static final String HBASE_PROPS = "/hbase.properties";
+    private static Properties props;
+
+    static {
+        try (InputStream stream = HBaseITCase.class.getResourceAsStream(HBASE_PROPS)) {
+            props = MiniClusterUtil.loadPropertiesStream(stream);
+        } catch (IOException e) {
+            LOG.error("Unable to load property file: {}", HBASE_PROPS);
+        }
+    }
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        MiniClusterUtil.startHBASE(props);
+    }
+
     @Test
-    public void testConnect(){
+    public void testConnect()throws Exception {
 
         //make a connection on localhost to spark
         SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
@@ -31,7 +58,10 @@ public class SparkHBaseITCase {
 
         ArrayList values = new ArrayList();
         for(int i=0;i<100;i++){values.add(i);}
-        //RDD rdd = sc.parallelize(values).map(i -> (i.toString(), i+1, "Hello"))
+        // rdd = sc.parallelize(values).map(x -> x);
+
+
+
 
 
 
