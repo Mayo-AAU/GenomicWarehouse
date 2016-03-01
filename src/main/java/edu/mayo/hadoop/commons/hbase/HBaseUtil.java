5af96cbbb433222774edb3c630cc45a5de3b852c
drachimera <daniel.quest@gmail.com>
2016-03-01 10:49:19 -0600

    adding the capability to add a table to hbase utility... this still needs a unit test

--- src/main/java/edu/mayo/hadoop/commons/hbase/HBaseUtil.java
+++ src/main/java/edu/mayo/hadoop/commons/hbase/HBaseUtil.java
@@ -2,9 +2,12 @@ package edu.mayo.hadoop.commons.hbase;
 
 import java.io.IOException;
 
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
+import org.apache.log4j.Logger;
 
 /**
  * Created by m102417 on 2/12/16.
@@ -13,6 +16,8 @@ import org.apache.hadoop.hbase.client.Connection;
  *
  */
 public class HBaseUtil {
+
+    static Logger logger = Logger.getLogger(HBaseUtil.class);
 
     private Connection connection;
 
@@ -31,6 +36,28 @@ public class HBaseUtil {
                 admin.deleteTable(tn);
             }
         }
+    }
+
+    /**
+     *
+     * @param tableName - the name of the new table
+     * @param familys   - the column families in the new table
+     * @throws Exception
+     */
+    public void createTable(String tableName, String[] familys) throws Exception {
+        Admin admin = connection.getAdmin();
+        final TableName table = TableName.valueOf(tableName);
+        if (admin.tableExists(table)) {
+            logger.warn("table already exists!");
+        } else {
+            HTableDescriptor tableDesc = new HTableDescriptor(table);
+            for (int i = 0; i < familys.length; i++) {
+                tableDesc.addFamily(new HColumnDescriptor(familys[i]));
+            }
+            admin.createTable(tableDesc);
+            logger.info("create table " + tableName + " ok.");
+        }
+        admin.close();
     }
 
 }
